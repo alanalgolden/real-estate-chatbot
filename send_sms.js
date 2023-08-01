ea00f56ffbe02401a2885d364b4a4e1703d88a2a
Alana Golden <Alanalgolden@gmail.com>
2023-08-01 00:46:08 -0400

    first commit

---
+++ send_sms.js
@@ -0,0 +1,12 @@
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const toNumber = process.env.TWILIO_TO_NUMBER;
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    body: "This is the ship that made the kessel Run in fourteen parsecs?",
+    from: "+18556020512",
+    to: `+${toNumber}`,
+  })
+  .then((message) => console.log(message.sid));
